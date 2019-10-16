e5b66fcc184f4a0d10f2f104cae5c4cb6bc88387
VeeDock <32637799+VeeDock@users.noreply.github.com>
2019-10-16 17:32:41 +0300

    Create enum.js

---
+++ enum.js
@@ -0,0 +1,28 @@
+function enumeration(namesToValues){
+				var enumeration = function (){throw "Нельзя создать экземпляр класса" + " Enumeration";};
+				
+				var proto = enumeration.prototype = {
+					constructor: enumeration, 
+					toString: function() {return this.name;},
+					valueOf: function() {return this.value; },
+					toJSON: function() { return this.name; }
+				};
+				
+				enumeration.values = [];
+				
+				for (name in namesToValues) {
+					var e = Object.create(proto);
+					e.name = name;
+					e.value = namesToValues[name];
+					enumeration[name] = e;
+					enumeration.values.push(e);
+				}
+				
+				enumeration.forEach = function(f,c) {
+					for (let i = 0; i<this.values.length; i++) f.call(c, this.values[i]);
+				};
+				
+				return enumeration;
+			}
+      
+ module.exports = enumeration;
